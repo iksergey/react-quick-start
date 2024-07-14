9312ce50f1461d5ebfdf0abddc57a0e907606ffa
Sergei Kamianetskii <i@ksergey.ru>
2024-07-15 02:48:41 +0300

    picsum.photos -> via.placeholder.com

--- src/Layout/HomePage/components/ContentItem.jsx
+++ src/Layout/HomePage/components/ContentItem.jsx
@@ -5,7 +5,7 @@ const ContentItem = (props) => {
         <div >
             {props.setOfLabels.map((item, index) =>
                 <div key={item.id} className="card m-3" >
-                    <img src={`https://picsum.photos/${1000 + index}/300`} className="card-img-top" alt="..." />
+                    <img src={`http://via.placeholder.com/640x${300 + index}`} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{item.title.slice(0, 30)}</h5>
                         <p className="card-text">{item.body.slice(0, 50)}</p>
